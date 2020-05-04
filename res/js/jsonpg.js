f426248653b1b88e0aab154e990c74b473d6e256
Gavin Ovsak <ovsak.gavin@gmail.com>
2020-05-04 16:14:40 -0400

    u

--- res/js/jsonpg.js
+++ res/js/jsonpg.js
@@ -1,11 +1,11 @@
 var jsonpg = {};
 
-var domain = 'localhost:1002';
-// var domain = 'jsonpg.herokuapp.com';
+// var domain = 'localhost:1002';
+var domain = 'jsonpg.herokuapp.com';
 
 var proxy = function(url) {
-  // return 'https://medmap.tech/proxy?url=' + encodeURIComponent(url);
-  return url;
+  return 'https://medmap.tech/proxy?url=' + encodeURIComponent(url);
+  // return url;
 
   // return 'http://localhost:3301/proxy?url=' + encodeURIComponent(url);
 }
